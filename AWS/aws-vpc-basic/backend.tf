2a86c5c332df29eaeebe16a84e51d01218249e32
Pavan Kiran Gummadi <pavankiran10@gmail.com>
2025-09-27 20:34:43 -0400

    Remove Untracked Files

--- AWS/aws-vpc-basic/backend.tf
+++ AWS/aws-vpc-basic/backend.tf
@@ -1,9 +1,2 @@
 terraform {
-  backend "s3" {
-    bucket         = "terraform-state-pavankiran222"
-    key            = "aws-vpc-basic/terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
-  }
 }
--- AWS/aws-vpc-basic/state-setup.tf
+++ AWS/aws-vpc-basic/state-setup.tf
@@ -1,9 +1,7 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-state-pavankiran222"
 
-  lifecycle {
-    prevent_destroy = true
-  }
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
